571d2fb92b93098c8153df996b77d4c09e60d428
石昌琦 <shicq@knownsec.com>
2019-09-04 13:45:52 +0800

    sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh 
+info=`pocsuite -r pocs -u 127.0.0.1`
+# echo "info: "${info}
+text="register" 
+result=$(echo $info | grep "${text}")
+if [[ "$result" != "" ]]
+then
+	echo "failed"
+	exit 1
+else
+	echo "success"
+	exit 0
+fi
