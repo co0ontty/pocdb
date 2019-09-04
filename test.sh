fd53dc895453f0ccf8255939fa776b44cf7ed7b5
石昌琦 <shicq@knownsec.com>
2019-09-04 13:52:09 +0800

    sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ info=`pocsuite -r pocs -u 127.0.0.1`
 # echo "info: "${info}
 text="register" 
 result=$(echo $info | grep "${text}")
-if [[ "$result" != "" ]]
+if [ "$result" != "" ]
 then
 	echo "failed"
 	exit 1
