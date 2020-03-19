15eafff74a527e329870ad7c93436a3e23c69d55
co0ontty <37443630+co0ontty@users.noreply.github.com>
2020-03-19 14:21:55 +0800

    Update .check_poc.sh

--- .check_poc.sh
+++ .check_poc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "---------poc check----------"
-poc_command=`pocsuite -r pocs -u 107.182.25.94:7001 `
+poc_command=`pocsuite -r pocs -u 127.0.0.1 `
 error_message="register"
 eval 'pocsuite -r pocs -f target.txt --thread 40'
 result_poc=$(echo $poc_command | grep "${error_message}")
